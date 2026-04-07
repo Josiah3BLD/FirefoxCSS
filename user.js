de58cb0c14680cd8ebcd04b8004fbe35a538b518
Josiah Gilmour <104062658+Josiah3BLD@users.noreply.github.com>
2026-04-08 01:18:24 +1000

    Update user.js
    Shortened the expand-on-hover delay
    Sped up the expand-on-hover animation

--- user.js
+++ user.js
@@ -3,5 +3,6 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets",true);
 user_pref("browser.tabs.groups.enabled",true);
 user_pref("sidebar.expandOnHover",true);
-user_pref("sidebar.animation.expand-on-hover.duration-ms",100);
+user_pref("sidebar.animation.expand-on-hover.delay-duration-ms",50);
+user_pref("sidebar.animation.expand-on-hover.duration-ms",150);
 user_pref("browser.profiles.enabled",true);
